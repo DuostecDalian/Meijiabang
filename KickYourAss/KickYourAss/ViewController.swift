676ca079a021d53effee95b7ad238efb9b79e1f1
icylydia <icylydia@gmail.com>
2015-01-19 13:49:35 +0800

    初始化工程

---
+++ KickYourAss/KickYourAss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KickYourAss
+//
+//  Created by eagle on 15/1/19.
+//  Copyright (c) 2015年 Li Chaoyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
