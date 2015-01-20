c6f48517aa35d4c84d9176e54db7dae7b4fe83f6
ChinaStart <zxy_start@outlook.com>
2015-01-20 10:40:12 +0800

    add blank

--- KickYourAss/KickYourAss/ZXY_Start/ZXY_SearchVCS/ZXY_NailSearchVC.swift
+++ KickYourAss/KickYourAss/ZXY_Start/ZXY_SearchVCS/ZXY_NailSearchVC.swift
@@ -19,6 +19,7 @@ class ZXY_NailSearchVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
 
