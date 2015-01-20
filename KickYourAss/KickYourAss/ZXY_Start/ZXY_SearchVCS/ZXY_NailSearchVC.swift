227e580bb2aa92dbdb79819ff94f1a713ea03670
ChinaStart <zxy_start@outlook.com>
2015-01-20 10:34:15 +0800

    zxyFirstCommity

---
+++ KickYourAss/KickYourAss/ZXY_Start/ZXY_SearchVCS/ZXY_NailSearchVC.swift
@@ -0,0 +1,35 @@
+//
+//  ZXY_NailSearchVC.swift
+//  KickYourAss
+//
+//  Created by 宇周 on 15/1/20.
+//  Copyright (c) 2015年 Li Chaoyi. All rights reserved.
+//
+
+import UIKit
+
+class ZXY_NailSearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
