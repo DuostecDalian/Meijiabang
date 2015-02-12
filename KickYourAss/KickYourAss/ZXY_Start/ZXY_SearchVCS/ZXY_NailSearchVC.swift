d1d1071e5cffb5f89e28a05d44910c1c5f99b9e8
ChinaStart <zxy_start@outlook.com>
2015-02-12 17:48:27 +0800

    哈哈

--- KickYourAss/KickYourAss/ZXY_Start/ZXY_SearchVCS/ZXY_NailSearchVC.swift
+++ KickYourAss/KickYourAss/ZXY_Start/ZXY_SearchVCS/ZXY_NailSearchVC.swift
@@ -13,7 +13,7 @@ import MapKit
 class ZXY_NailSearchVC: UIViewController {
     var toHeaderMax: CGFloat = 239.0
     let mapScale   : CLLocationDegrees = 0.001
-    
+    var isFirst = true
     var locService = BMKLocationService()
     //var anno: ZXY_BMKAnnotation?
     var annos : [ZXY_BMKAnnotation]! = []
@@ -66,7 +66,15 @@ class ZXY_NailSearchVC: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        currentMap.frame = self.view.frame
+        if(isFirst)
+        {
+            currentMap.frame = self.view.frame
+            isFirst = false
+        }
+        else
+        {
+        
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
